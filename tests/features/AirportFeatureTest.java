5f1243f679b11845640ff6abd9c812fca8eec279
Erin Stevenson <estevenson_94@live.com>
2019-03-07 22:57:41 +0000

    Weather mocked in feature test

--- tests/features/AirportFeatureTest.java
+++ tests/features/AirportFeatureTest.java
@@ -1,7 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AirportFeatureTest {
 
@@ -9,14 +12,21 @@ public class AirportFeatureTest {
     private Plane plane;
     private Weather weather;
 
+
+    @Mock
+    private Weather mockedWeather;
+
+
     @Before
     public void beforeEachFeatureTest() {
-        this.airport = new Airport(weather, 10);
+        mockedWeather = mock(Weather.class);
+        this.airport = new Airport(mockedWeather, 10);
         this.plane = new Plane();
     }
 
     @Test
     public void planeCanTakeOffFromAirport() throws WeatherException, AirportFullException {
+        when(mockedWeather.isStormy()).thenReturn(false);
         airport.takeOff(plane);
         assertFalse(plane.landed);
         assertFalse(airport.hasPlane(plane));
@@ -24,6 +34,7 @@ public class AirportFeatureTest {
 
     @Test
     public void planeCanLandAtAirport() throws WeatherException, AirportFullException {
+        when(mockedWeather.isStormy()).thenReturn(false);
         airport.takeOff(plane);
         airport.land(plane);
         assertTrue(plane.landed);
